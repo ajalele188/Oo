22d55fdfc14dea10911a545d7becad82b5ccbf77
ajalele188 <byeok485@gmail.com>
2025-05-02 01:21:39 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 27881923
-api_hash = "79abda5e46a51fc0dce1313f2548ce19"
-bot_token = "6263666911:AAF_Zm2baKNCJMGPn8aCBlVArvTa-A4kz1k"
+api_id = 28748671
+api_hash = "f53ec7c41ce34e6d585674ed9ce6167c"
+bot_token = "7791056995:AAGTt6xsnHrcST6V496hzHlht1RpO-be1lc"
 
